e949b0c8528577d7524c408707f7bab781177e30
dcollarana <RadOffice01Git>
2019-03-21 18:15:36 +0100

    First version

---
+++ main.py
@@ -0,0 +1,55 @@
+import logging
+import os
+import sys
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pykeen
+
+%matplotlib inline
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('pykeen').setLevel(logging.INFO)
+
+print(sys.version)
+print(time.asctime())
+print(pykeen.get_version())
+
+output_directory = os.path.join(
+    os.path.expanduser('~'),
+    'Desktop',
+    'pykeen_test'
+)
+
+config = dict(
+    training_set_path           = '/Users/mehdi/PycharmProjects/PyKEEN/data/corpora_local/rdf.nt',
+    execution_mode              = 'Training_mode',
+    random_seed                 = 0,
+    kg_embedding_model_name     = 'TransE',
+    embedding_dim               = 50,
+    scoring_function            = 2,  # corresponds to L2
+    margin_loss                 = 0.05,
+    weigthing_soft_constraint   = 0.015625,
+    learning_rate               = 0.01,
+    num_epochs                  = 10,
+    batch_size                  = 64,
+    preferred_device            = 'gpu'
+)
+
+results = pykeen.run(
+    config=config,
+    output_directory=output_directory,
+)
+
+results.results.keys()
+
+results.results['trained_model']
+
+losses = results.results['losses']
+epochs = np.arange(len(losses))
+plt.title(r'Loss Per Epoch')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.plot(epochs, losses)
+plt.show()
