49d5d7ebfcd96a01c22f626f1a92ebbcc7da37a5
dcollarana <RadOffice01Git>
2019-03-22 12:09:29 +0100

    Small fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pykeen
 
-%matplotlib inline
+#%matplotlib inline
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('pykeen').setLevel(logging.INFO)
